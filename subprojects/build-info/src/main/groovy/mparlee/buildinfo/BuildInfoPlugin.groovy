59152c518e13e617f06209c92a1e8df704ae50b0
Michael Parlee <michael.parlee@dexcom.com>
2021-02-11 09:48:01 -0800

    changes added

--- subprojects/build-info/src/main/groovy/mparlee/buildinfo/BuildInfoPlugin.groovy
+++ subprojects/build-info/src/main/groovy/mparlee/buildinfo/BuildInfoPlugin.groovy
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package groovycalamari.buildinfo
+package mparlee.buildinfo
 
 import groovy.transform.CompileStatic
 import org.gradle.api.Action
